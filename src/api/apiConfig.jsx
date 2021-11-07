d24b74932eda4eac4f083cca85dea2b22b4c3dce
khanguyen01it <kha.nguyen01.it@gmail.com>
2021-11-07 15:56:14 +0700

    config api

---
+++ src/api/apiConfig.jsx
@@ -0,0 +1,8 @@
+const apiConfig = {
+    baseUrl: 'https://api.themoviedb.org/3/',
+    apiKey: process.env.TMDB_API_KEY,
+    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+}
+
+export default apiConfig;
