ab785de8a41ec62b55dfa752af16fa8c4a97772a
axnguyenit <axnguyen.it@gmail.com>
2022-07-06 03:36:43 +0700

    update files name

---
+++ src/components/cast/CastItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { imageApi } from '../../api';
+
+// ----------------------------------------------------------------------
+
+export default function CastItem({ cast }) {
+  const background = imageApi.w500Image(cast.profile_path);
+  return (
+    <div className="relative movie__item h-44 bg-cover bg-center bg-no-repeat group duration-200 my-3 mx-3 shadow-lg hover:shadow-lg border-l-0 hover:border-l-4 border-red-600 overflow-hidden rounded-md">
+      <div className="absolute flex top-0 left-0 w-full h-full">
+        <img src={background} className="w-2/5 h-full object-cover" alt="" />
+        <div className="text-gray-300 h-full p-5">
+          <div className="line-6">
+            <h3 className="text-lg">{cast.name}</h3>
+            <h4 className="text-red-600 font-semibold">{cast.character}</h4>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-0 left-0 w-full h-full overlay overlay-2" />
+    </div>
+  );
+}
